9f1487e0af475a85fee017f2b4bc0d2cd0ae2198
agustinvd10 <avalenzu@chicagobooth.edu>
2021-03-03 00:02:47 +0000

    photo is working

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_03_000213) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "caption"
+    t.string "image"
+    t.integer "owner_id"
+    t.integer "likes_count"
+    t.integer "comments_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
